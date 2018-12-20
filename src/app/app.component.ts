6f34c5ed9e35528ad585fd35c19c461e3ecc5f62
Marcel Good <marcelg@ideablade.com>
2018-12-20 11:18:40 -0800

    Simple example to demonstrate the tagbox issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import DataSource from 'devextreme/data/data_source';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'devextreme-tagbox-issue';
+
+  dataSource: DataSource;
+  selectedItems = [];
+
+  // Items with circular dependencies to demonstrate the tagbox issue
+  items = [
+    { name: 'One', owner: this },
+    { name: 'Two', owner: this },
+    { name: 'Three', owner: this }
+  ];
+
+  ngOnInit() {
+    this.selectedItems = [this.items[0]];
+
+    this.dataSource = new DataSource({
+      load: loadOptions => this.items,
+      byKey: key => key
+    });
+  }
 }
